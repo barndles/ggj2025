986d43300d1e98cdab13c15631b55b0ddb84e19a
barndles <barndles@gmail.com>
2025-01-25 10:54:00 -0600

    bumper logic
    it was easy im just dumpby

--- Scenes/Bumpers/bumper.gd
+++ Scenes/Bumpers/bumper.gd
@@ -1,11 +1,10 @@
 extends Node2D
 
-@onready var collision_shape_2d: CollisionShape2D = $StaticBody2D/CollisionShape2D
 @onready var bumper: Node2D = $"."
 
 var launch_angle = Vector2(0,0)
 var launch_vector = Vector2(0,0)
-var launch_power = 10
+@export var launch_power = 10
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -16,9 +15,14 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	launch_angle = bumper.global_position.angle_to(body.global_position)
+func _on_area_2d_body_entered(body: RigidBody2D) -> void:
+
+	launch_angle = (body.global_position - bumper.global_position).normalized()
 	launch_vector = launch_angle * launch_power
 	
-	
+	if body == null:
+		return
+	else:
+		print(launch_vector)
+		body.apply_impulse(launch_vector)
 	

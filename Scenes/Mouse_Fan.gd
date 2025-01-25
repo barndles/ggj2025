9af081c38f67e229944f0db5fcd529e0914146ff
barndles <barndles@gmail.com>
2025-01-25 13:55:23 -0600

    FAN
    IT TWERKS

--- Scenes/Mouse_Fan.gd
+++ Scenes/Mouse_Fan.gd
@@ -3,9 +3,11 @@ extends Node2D
 @export var Target:RigidBody2D
 
 var mouse_position = Vector2(0,0)
-var mouse_previous_frame_position = Vector2(0,0)
+var local_mouse_position_previous_frame = Vector2(0,0)
 var ball_position = Vector2(0,0)
 var blowing_vector = Vector2(0,0)
+
+var ct = 0
 
 var mouse_speed = 0
 @export var blowing_power = 0.01
@@ -18,17 +20,24 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	
 	if Target == null:
 		return
+
+	ct += 1
+	if ct < 5:
+		mouse_position = get_global_mouse_position()
+		local_mouse_position_previous_frame = get_local_mouse_position()
+		ball_position = Target.global_position
+		return
+		
+		
 	mouse_position = get_global_mouse_position()
 	ball_position = Target.global_position
 	
-	mouse_speed = mouse_position.distance_to(mouse_previous_frame_position)
-	blowing_vector = (ball_position - mouse_position).normalized() * (mouse_speed * blowing_power)
+	mouse_speed = abs(get_local_mouse_position() - local_mouse_position_previous_frame)
+	blowing_vector = (ball_position - mouse_position).normalized() * mouse_speed
 	
-	Target.add_constant_force(blowing_vector)
+	Target.apply_force(blowing_vector * blowing_power)
 	
-	mouse_previous_frame_position = mouse_position
-
-func _draw() -> void:
-	draw_line(ball_position, ball_position+blowing_vector, Color(0,0,0))
+	local_mouse_position_previous_frame = get_local_mouse_position()
